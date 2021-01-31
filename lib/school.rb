9087639960a69f8a2ccb7e792fd4434c1cd5c181
Mike Nanto <mike@thecowboymechanic.com>
2021-01-31 18:18:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  attr_accessor :name, :roster    
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+  
+  def grade(school_grade)
+    roster[school_grade]
+  end
+  
+end
